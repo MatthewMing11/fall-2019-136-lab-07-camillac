2818c29371eab5af49de52828a3ef9e36762e5bb
MatthewMing11 <Matthew.Ming11@myhunter.cuny.edu>
2019-11-04 12:11:33 -0500

    added tests to tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -8,3 +8,7 @@ TEST_CASE("INDENT"){
   std::string fixed = "int main(){\n\t// Hi, I'm a program!\n\tint x = 1;\n\tfor(int i = 0; i < 10; i++) {\n\t\tcout << i;\n\t\tcout << endl;\n\t}\n}";
   CHECK(indent("bad-code.cpp")== fixed);
 }
+TEST_CASE("COMMENT"){
+   std::string correct = "int main(){\n\t// {{\n\tif(A){\n\t\t/*{{{{{{\n\t\t}\n\t\t*/\n\t\t//}\n\t}\n}";
+   CHECK(indent("testfile")==correct);
+}
